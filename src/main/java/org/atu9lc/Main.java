ec6306bf75a7097def3bc935c201f58a67a7f76c
Alan Tu <alantu9000@hotmail.com>
2021-05-31 11:26:44 -0400

    Initial commit

---
+++ src/main/java/org/atu9lc/Main.java
@@ -0,0 +1,10 @@
+package org.atu9lc;
+
+import org.atu9lc.lc0.LC001;
+
+public class Main {
+    public static void main(String[] args) {
+        LC001 lc001 = new LC001();
+        lc001.foo();
+    }
+}
---
+++ src/main/java/org/atu9lc/lc0/LC001.java
@@ -0,0 +1,8 @@
+package org.atu9lc.lc0;
+
+public class LC001 {
+    public void foo() {
+        System.out.println("lc001 foo");
+    }
+
+}
